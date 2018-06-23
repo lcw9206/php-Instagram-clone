dc7a75fc6fb924525d99ceb911480fda93fd0dfd
Lee <lcw9206@gmail.com>
2018-06-23 11:27:37 +0900

    Connect controller and view

--- application/controllers/Board.php
+++ application/controllers/Board.php
@@ -3,16 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Board extends CI_Controller {
 
-	public function index()
-	{
+	public function index()	{
 		// $sql = "SELECT * FROM posts WHERE is_secret != 'Y' ORDER BY created DESC";
 		// $result = mysqli_query($link, $sql);
 
 		$this->db->where('is_secret !=', 'Y');
 		$this->db->order_by('created', 'desc');
 		$posts = $this->db->get('posts')->result_array();
-		print_r($posts);
+		$data['posts'] = $posts;	# key, value 값 설정을 통해 $data를 이용해 손쉽게 view로 데이터를 보낼 수 있다.
 
-		$this->load->view('board');
+		$this->load->view('board', $data);
 	}
 }

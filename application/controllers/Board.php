e5aa0a731b59d5287490670ac8fecb9f5b9679ec
Lee <lcw9206@gmail.com>
2018-06-23 11:46:42 +0900

    Connect header & footer to view

--- application/controllers/Board.php
+++ application/controllers/Board.php
@@ -3,15 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Board extends CI_Controller {
 
-	public function index()	{
+	public function index()	
+	{
 		// $sql = "SELECT * FROM posts WHERE is_secret != 'Y' ORDER BY created DESC";
 		// $result = mysqli_query($link, $sql);
 
 		$this->db->where('is_secret !=', 'Y');
 		$this->db->order_by('created', 'desc');
 		$posts = $this->db->get('posts')->result_array();
-		$data['posts'] = $posts;	# key, value 값 설정을 통해 $data를 이용해 손쉽게 view로 데이터를 보낼 수 있다.
+		$data['posts'] = $posts;	# key, value 값 설정으로 $data를 이용해 손쉽게 view로 데이터를 보낼 수 있다.
+		$data['header'] = $this->_header();	# 헤더파일 불러오기.
+		$data['footer'] = $this->_footer();
 
 		$this->load->view('board', $data);
 	}
+
+	public function _header()	# 메서드 앞에 '_'를 붙이면 외부에서 접근이 불가능하다. 주로 설정파일 같은 것들에 사용한다.
+	{
+		return $this->load->view('_header','',true);	# true를 하면 값을 뿌려주는 것이 아닌 값을 반환(return)한다.
+	}
+
+	public function _footer()
+	{
+		return $this->load->view('_footer','',true);
+	}
+
 }
--- application/views/board.php
+++ application/views/board.php
@@ -1,3 +1,31 @@
-<?php
-	print_r($posts);
-?>
+<?=$header?>
+
+<div class="bs-example" data-example-id="simple-table">
+   	<table class="table">
+      	<thead>
+        	<tr>
+	            <th>번호</th>
+	            <th>제목</th>
+	            <th>내용</th>
+	            <th>생성일</th>
+	            <th></th>
+	        </tr>
+      	</thead>
+      	<tbody>
+      		<?php foreach($posts as $post) : ?>
+	        <tr>
+	            <td><?=$post['no']?></td>
+	            <td><a href="/board/view.php?no=<?=$post['no']?>"><?=$post['title']?></a></td>
+	            <td><?=$post['content']?></td>
+	            <td><?=$post['created']?></td>
+	            <td><a href="/board/write.php?no=<?=$post['no']?>" class="btn btn-primary btn-sm">수정</td>
+	        </tr>
+	     	<?php endforeach; ?>
+    	</tbody>
+   	</table>
+	<div>
+		<a href="/board/write.php" class='btn btn-primary'>글쓰기</a>
+	</div>
+</div>
+
+<?=$footer?>

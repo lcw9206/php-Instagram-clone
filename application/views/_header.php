bb99fb94dbe0d64c9f329457922c1ffed8d00c24
Lee <lcw9206@gmail.com>
2018-07-14 17:09:21 +0900

    Add static files folder & Add link to header.php

--- application/views/_header.php
+++ application/views/_header.php
@@ -1,11 +1,13 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>게시판</title>
+		<title>인스타그램</title>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="/assets/css/web.css" />
+
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
 	</head>
 
 	<body>
 		<div class="container">
-			<h3 class="text-center">나의 게시판</h3>
